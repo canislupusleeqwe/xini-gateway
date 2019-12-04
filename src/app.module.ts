496274876f80d7dcada2f62f7e8bc9504ae3f897
canislupusleeqwe <canislupusleeqwe@gmail.com>
2019-12-04 19:34:26 +0800

    Lee:切面

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-
+// import { AppController } from './app.controller';
+// import { AppService } from './app.service';
+import { FeaturesModule } from './features/features.module';
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [FeaturesModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
---
+++ src/features/apis/cats/cats.controller.ts
@@ -0,0 +1,12 @@
+import { Controller, Get, Param } from '@nestjs/common';
+
+import { CatsService } from './cats.service';
+
+@Controller('cats')
+export class CatsController {
+  constructor(private readonly catsService: CatsService) {}
+  @Get(':id')
+  findOne(@Param('id') id: string): string {
+    return this.catsService.getCat(id);
+  }
+}
